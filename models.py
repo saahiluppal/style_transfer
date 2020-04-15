04c6a1adf577d622ebb7c0347931e9a90583a3cd
Sahil Uppal <sahiluppal2k@gmail.com>
2020-04-15 11:59:17 +0530

    updated

--- models.py
+++ models.py
@@ -1,26 +1,6 @@
 import tensorflow as tf
 
 
-class ContentModel(tf.keras.Model):
-    def __init__(self, content_layers):
-        super(ContentModel, self).__init__()
-        vgg = tf.keras.applications.VGG19(
-            include_top=False, weights='imagenet',
-            pooling='avg'
-        )
-        outputs = [vgg.get_layer(name).output
-                   for name in content_layers]
-
-        self.model = tf.keras.Model(vgg.input, outputs)
-        self.model.trainable = False
-
-    def call(self, x):
-        x = x * 255
-        x = tf.keras.applications.vgg19.preprocess_input(x)
-        output = self.model(x)
-        return output
-
-
 class StyleModel(tf.keras.Model):
     def __init__(self, style_layers):
         super(StyleModel, self).__init__()
@@ -48,3 +28,23 @@ class StyleModel(tf.keras.Model):
         output = self.model(x)
         gm = [self.gram_matrix(val) for val in output]
         return gm
+
+
+class ContentModel(tf.keras.Model):
+    def __init__(self, content_layers):
+        super(ContentModel, self).__init__()
+        vgg = tf.keras.applications.VGG19(
+            include_top=False, weights='imagenet',
+            pooling='avg'
+        )
+        outputs = [vgg.get_layer(name).output
+                   for name in content_layers]
+
+        self.model = tf.keras.Model(vgg.input, outputs)
+        self.model.trainable = False
+
+    def call(self, x):
+        x = x * 255
+        x = tf.keras.applications.vgg19.preprocess_input(x)
+        output = self.model(x)
+        return output
